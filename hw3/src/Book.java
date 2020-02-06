b0e749aec958a1d054ad00b8d88134f01b56d5eb
levoschkin <levoschkin@IGORILIN>
2020-02-06 12:26:37 +0300

    ДЗ 3

---
+++ hw3/src/Book.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Book {
+    HashMap<String, HashSet<String>> pb;
+
+    public Book() {
+        this.pb = new HashMap<>();
+    }
+
+    public void addContact(String name, String phone) {
+        HashSet<String> book = pb.getOrDefault(name, new HashSet<>());
+        book.add(phone);
+        pb.put(name, book);
+    }
+
+    public void findAndPrint(String name) {
+        System.out.println("Contact " + name + " / Phone number " + pb.getOrDefault(name, new HashSet<>()));
+    }
+}
